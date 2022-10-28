59f057d01d138185d43e6a71f3b584c8ee31d042
x_coding <l2xguard@live.com>
2022-10-29 02:25:04 +0300

    remove unused stuff

--- CIARELiveShareAPI/Program.cs
+++ CIARELiveShareAPI/Program.cs
@@ -11,7 +11,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<IUserIdProvider, NameUserIdProvider>();
 builder.Services.AddResponseCompression(opts =>
 { 
     opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(

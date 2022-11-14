ce654395af3b8c6f2dbcbdcbb0cdd0aa3f20203f
x_coding <l2xguard@live.com>
2022-11-14 21:59:50 +0200

    add: new variable for live host code display on remote on first connection

--- CIARELiveShareAPI/Utils/GlobalVariables.cs
+++ CIARELiveShareAPI/Utils/GlobalVariables.cs
@@ -3,5 +3,6 @@
     public class GlobalVariables
     {
         public static List<string> listKeys = new List<string>();
+        public static Dictionary<string, string> hostData = new Dictionary<string, string>();
     }
 }
